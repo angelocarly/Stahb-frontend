883b89506a7e28783f535a541be3c6405e6f9ba4
angelocarly <angelo.carly@protonmail.com>
2020-01-10 18:33:53 +0100

    Add bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TabListComponent } from './tab-list/tab-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabListComponent
   ],
   imports: [
+    NgbModule,
     BrowserModule,
     AppRoutingModule
   ],
---
+++ src/app/tab-list/tab-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tab-list',
+  templateUrl: './tab-list.component.html',
+  styleUrls: ['./tab-list.component.css']
+})
+export class TabListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
