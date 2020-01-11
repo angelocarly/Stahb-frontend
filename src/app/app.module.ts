a5e4d854d8b459722ab36a91ab6597d759431e32
angelocarly <angelo.carly@protonmail.com>
2020-01-11 20:18:22 +0100

    Add basic tab listing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule }    from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TabListComponent } from './tab-list/tab-list.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TabListComponent
+    TabListComponent,
+    NavbarComponent
   ],
   imports: [
     NgbModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/models/Tab.ts
@@ -0,0 +1,6 @@
+export class Tab {
+    id: string;
+    artist: string;
+    song: string;
+    tab?: string;
+}
