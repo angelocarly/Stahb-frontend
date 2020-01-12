1bb1af87837cfb492278797e7dfb660eae599405
angelocarly <angelo.carly@protonmail.com>
2020-01-12 16:36:26 +0100

    Fix router link

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TabListComponent } from './tab-list/tab-list.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { TabDetailsComponent } from './tab-details/tab-details.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: 'tab', component: TabDetailsComponent },
+  { path: 'tabs', component: TabListComponent },
+  { path: '', redirectTo: 'tabs', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     TabListComponent,
-    NavbarComponent
+    NavbarComponent,
+    TabDetailsComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: false } //<-- debugging purposes
+    ),
     NgbModule,
     BrowserModule,
     AppRoutingModule,
---
+++ src/app/tab-details/tab-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tab-details',
+  templateUrl: './tab-details.component.html',
+  styleUrls: ['./tab-details.component.scss']
+})
+export class TabDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
